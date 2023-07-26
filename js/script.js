4e2387b95f354d935db7177cea849c5664671fd2
agustinaojeda <102766633+agustinaojeda@users.noreply.github.com>
2023-07-26 13:38:34 -0300

    inicio
    cartas creadas y con la interaccion inicial

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log("hola");
+let grupoTarjetas = ["🐰", "🦊", "🐻", "🐼", "🐭", "🐶", "🐱", "🦄"];
+let totalTarjetas = grupoTarjetas.concat(grupoTarjetas);
+
+function repartirTarjetas() {
+
+    let mesa = document.querySelector('#mesa');
+
+    mesa.innerHTML = "";
+
+    totalTarjetas.forEach(function (elemento, indice) {
+        let tarjeta = document.createElement("div");
+
+        tarjeta.innerHTML = "<div class='tarjeta'>" + "<div class='tarjetaContenido'>" +
+            elemento + "</div>" + "</div>";
+
+        mesa.appendChild(tarjeta);
+    })
+}
+
+function descubrir() {
+    this.classList.add("descubierta");
+}
+
+repartirTarjetas();
+
+document.querySelectorAll(".tarjeta").forEach(function (elemento) {
+    elemento.addEventListener("click", descubrir);
+})
