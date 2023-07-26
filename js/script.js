abf74a12a682ca5846a150fd752c6e18465e6af6
agustinaojeda <102766633+agustinaojeda@users.noreply.github.com>
2023-07-26 15:38:37 -0300

    comparacion e interaccion basica

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 let grupoTarjetas = ["🐰", "🦊", "🐻", "🐼", "🐭", "🐶", "🐱", "🦄"];
 let totalTarjetas = grupoTarjetas.concat(grupoTarjetas);
 
+function barajarTarjetas() {
+    let resultado = totalTarjetas.sort(function () {
+        return 0.5 - Math.random();
+    });
+
+    return resultado;
+}
+
 function repartirTarjetas() {
 
     let mesa = document.querySelector('#mesa');
+    let tarjetasBarajadas = barajarTarjetas();
 
     mesa.innerHTML = "";
 
-    totalTarjetas.forEach(function (elemento, indice) {
+    tarjetasBarajadas.forEach(function (elemento, indice) {
         let tarjeta = document.createElement("div");
 
-        tarjeta.innerHTML = "<div class='tarjeta'>" + "<div class='tarjetaContenido'>" +
+        tarjeta.innerHTML = "<div class='tarjeta' data-valor= " + elemento + ">" + "<div class='tarjetaContenido'>" +
             elemento + "</div>" + "</div>";
 
         mesa.appendChild(tarjeta);
@@ -18,7 +27,42 @@ function repartirTarjetas() {
 }
 
 function descubrir() {
+    let descubiertas;
+    let totalDescubiertas = document.querySelectorAll(".descubierta:not(.acertada)");
+
+    if (totalDescubiertas.length > 1) {
+        return;
+    }
+
     this.classList.add("descubierta");
+
+    descubiertas = document.querySelectorAll(".descubierta:not(.acertada)");
+    if (descubiertas.length > 2) {
+        return;
+    } else if (descubiertas.length == 2) {
+        comparar(descubiertas);
+    }
+
+}
+
+function comparar(tarjetasAComparar) {
+    if (tarjetasAComparar[0].getAttribute('data-valor') === tarjetasAComparar[1].getAttribute('data-valor')) {
+        acierto(tarjetasAComparar);
+    } else {
+        error(tarjetasAComparar);
+    }
+}
+
+function acierto(tarjetasAComparar) {
+    tarjetasAComparar.forEach(function (elemento) {
+        elemento.classList.add("acertada");
+    })
+}
+
+function error(tarjetasAComparar) {
+    tarjetasAComparar.forEach(function (elemento) {
+        elemento.classList.remove("descubierta");
+    })
 }
 
 repartirTarjetas();
